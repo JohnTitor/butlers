21f7f2ad53ad83994c8a50af81a3d78769d193b7
Yuki Okushi <jtitor@2k36.org>
2022-10-03 23:29:46 +0900

    Use v1.0 API for convenience
    Signed-off-by: Yuki Okushi <jtitor@2k36.org>

--- sebastian/src/main.rs
+++ sebastian/src/main.rs
@@ -6,9 +6,7 @@ use serde::Deserialize;
 fn main() {
     let device_id = env::var("DEVICE_ID").expect("DEVICE_ID must be set");
     let api_key = env::var("API_KEY").expect("API_KEY must be set");
-    let t = env::var("T").expect("T must be set");
-    let sign = env::var("SIGN").expect("SIGN must be set");
-    if let Err(e) = check_current_temp(&device_id, &api_key, &t, &sign) {
+    if let Err(e) = check_current_temp(&device_id, &api_key) {
         eprintln!("check_current_temp error: {}", e);
     }
 }
@@ -31,26 +29,13 @@ pub struct GetMeterPlusStatusResponseBody {
 fn check_current_temp(
     device_id: &str,
     api_key: &str,
-    t: &str,
-    sign: &str,
 ) -> Result<(), reqwest::Error> {
     let client = reqwest::blocking::Client::new();
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-        header::AUTHORIZATION,
-        header::HeaderValue::from_str(api_key).unwrap(),
-    );
-    headers.insert("t", header::HeaderValue::from_str(t).unwrap());
-    headers.insert(
-        "sign",
-        header::HeaderValue::from_str(sign).unwrap(),
-    );
-    headers.insert("nonce", header::HeaderValue::from_static(""));
     let resp = client
         .get(format!(
-            "https://api.switch-bot.com/v1.1/devices/{device_id}/status"
+            "https://api.switch-bot.com/v1.0/devices/{device_id}/status"
         ))
-        .headers(headers)
+        .header(header::AUTHORIZATION, api_key)
         .send()?
         .json::<GetMeterPlusStatusResponse>()?;
     println!("Current temperature: {:.1}", resp.body.temperature);
