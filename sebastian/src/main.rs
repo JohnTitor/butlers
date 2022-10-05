787b8e33165d661eb920b7e8b6a1c1e415e6086a
Yuki Okushi <jtitor@2k36.org>
2022-10-06 00:03:50 +0900

    feat: Record also now time
    Signed-off-by: Yuki Okushi <jtitor@2k36.org>

--- sebastian/src/main.rs
+++ sebastian/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs::OpenOptions};
 
+use anyhow::{Context, Result};
 use reqwest::header;
 use serde::{Deserialize, Serialize};
 
@@ -19,29 +20,51 @@ pub struct GetMeterPlusStatusResponse {
     pub message: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct GetMeterPlusStatusResponseBody {
     pub temperature: f32,
     pub humidity: u8,
 }
 
-fn record_current_temp(
-    device_id: &str,
-    api_key: &str,
-) -> Result<(), reqwest::Error> {
+#[derive(Serialize)]
+pub struct CsvEntity {
+    pub temperature: f32,
+    pub humidity: u8,
+    pub time: String,
+}
+
+fn record_current_temp(device_id: &str, api_key: &str) -> Result<()> {
     let client = reqwest::blocking::Client::new();
     let resp = client
         .get(format!(
             "https://api.switch-bot.com/v1.0/devices/{device_id}/status"
         ))
         .header(header::AUTHORIZATION, api_key)
-        .send()?
-        .json::<GetMeterPlusStatusResponse>()?;
+        .send()
+        .context("Failed to make a successful request")?
+        .json::<GetMeterPlusStatusResponse>()
+        .context("Failed to deserialize the response")?;
 
-    let file = OpenOptions::new().write(true).create(true).append(true).open("./data/temp_humidity.csv").unwrap();
-    let mut wtr = csv::WriterBuilder::new().has_headers(false).from_writer(file);
-    wtr.serialize(resp.body).unwrap();
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("./data/temp_humidity.csv")
+        .context("Failed to open CSV")?;
+    let mut wtr = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_writer(file);
+    let time_format = time::format_description::parse("[year]-[month]-[day] [hour]:[minute]")?;
+    wtr.serialize(CsvEntity {
+        temperature: resp.body.temperature,
+        humidity: resp.body.humidity,
+        time: time::OffsetDateTime::now_utc()
+            .to_offset(time::macros::offset!(+9))
+            .format(&time_format)
+            .context("Failed to prepare the recording time")?,
+    })
+    .context("Failed to serialize the data")?;
     Ok(())
 }
 
